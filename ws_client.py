164ac3282ae541dd65d0de3a61a892b96da1b989
Sjors Holtrop <sjors@sholtrop.dev>
2024-05-14 13:53:26 +0200

    switch to websocketapp for built-in reconnection logic

--- ws_client.py
+++ ws_client.py
@@ -4,32 +4,20 @@ import threading
 from time import sleep
 
 class RobotWebSocketConnection:
-
-    ws: websocket.WebSocket
+    ws: websocket.WebSocketApp
+    t: threading.Thread
 
     def __init__(self):
-        self.ws = websocket.WebSocket()
-        self._connect()
+        self.ws = websocket.WebSocketApp('ws://localhost:8765', on_message=print, on_error=print)
+        self.t = threading.Thread(target=self.ws.run_forever, daemon=True)
+        self.t.start()
+        sleep(0.1)
         print('Connected to robot websocket server')
-
-    def _connect(self):
-        self.ws.connect("ws://localhost:8765")
 
     # Where command is one of: 'forward', 'backward', 'stop', 'left', or 'right'
     def send_command(self, command: str):
         print('Sending command', command)
-        attempts = 3
-        while attempts > 0:
-            try:
-                self.ws.send_text(command)
-                return
-            except Exception as e:
-                print('error:', e, type(e))
-                attempts -= 1
-                print(f'trying to reconnect (attempt {3 - attempts})')
-                self._connect()
-        print('Could not reconnect. Exiting.')
-        exit(1)
+        self.ws.send_text(command)
 
 if __name__ == '__main__':
     robot_ws = RobotWebSocketConnection()
